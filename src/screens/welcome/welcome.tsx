dcb8794084c254911732250d203c453c81027f35
Guilherme Ytalo <gydaytalo@gmail.com>
2021-10-18 21:27:20 -0300

    creation of welcome page with button

--- src/screens/welcome/welcome.tsx
+++ src/screens/welcome/welcome.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Dimensions,
   Image,
+  ImageBackground,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -10,11 +11,11 @@ import {
 } from 'react-native';
 
 // import Feather from 'react-native-vector-icons/Feather';
-import {useNavigation} from '@react-navigation/core';
+import { useNavigation } from '@react-navigation/core';
 
-// import wateringImg from '../assets/watering.png';
-// import colors from '../styles/colors';
-// import fonts from '../styles/fonts';
+import dogImage from "../../assets/welcome-screen/welcome.jpg";
+import colors from '../../styles/color';
+import fonts from '../../styles/fonts';
 
 // Feather.loadFont();
 
@@ -22,18 +23,68 @@ export function Welcome() {
   const navigation = useNavigation();
 
   function handleStart() {
+    console.log('caiu?');
     // navigation.navigate('UserIdentification');
   }
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>
-          Gerencie {'\n'}
-          suas plantas de{'\n'}
-          forma fácil
-        </Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <ImageBackground style={styles.image} source={dogImage} resizeMode="cover">
+          <Text style={styles.title}>
+            Welcome to{'\n'}
+            PetWalk
+          </Text>
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.7}
+            onPress={handleStart}>
+              <Text style={styles.buttonText}>
+                Let's Walk
+              </Text>
+          </TouchableOpacity>
+        </ImageBackground>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    color: colors.primary,
+    marginTop: 38,
+    marginLeft: 38,
+    lineHeight: 34,
+    fontFamily: fonts.heading,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+  button: {    
+    backgroundColor: colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // alignContent: 'space-around',
+    // position: 'absolute',
+    borderRadius: 16,    
+    width: 200,
+    height: 56,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: fonts.complement,
+    color: colors.white
+  }
+})
---
+++ src/styles/color.ts
@@ -0,0 +1,4 @@
+export default {
+    primary: '#483951',
+    white: '#edefe6',
+}
